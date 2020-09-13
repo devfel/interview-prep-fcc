3899dff11656ab4a053ccdb1e440bcca8427fb50
Felizardo <devfelizardo@gmail.com>
2020-09-13 03:21:18 -0300

    some more interview prep javascript

--- js/javascript-intermediate.js
+++ js/javascript-intermediate.js
@@ -247,3 +247,66 @@ function translatePigLatin(str) {
     .replace(/^[aeiou]\w*/, "$&way")
     .replace(/(^[^aeiou]+)(\w*)/, "$2$1ay");
 }
+
+// Exerc-07 ---------------------------------- //
+// Search and Replace
+// Perform a search and replace on the sentence using the arguments provided and return the new sentence. First argument is the sentence to perform the search and replace on. Second argument is the word that you will be replacing (before). Third argument is what you will be replacing the second argument with (after).
+function myReplace(str, before, after) {
+  let strAux = str.split(" ");
+  let indexOfSearch = strAux.indexOf(before);
+
+  // put the after keyword always in lower case before using it.
+  after = after.toLowerCase();
+
+  //check if the word to be replaced was found.
+  if (indexOfSearch != -1) {
+    //check if the first letter of the word is capitalized
+    if (strAux[indexOfSearch][0].match(/[A-Z]/)) {
+      strAux[indexOfSearch] = after;
+      let wordSearched = strAux[indexOfSearch].split("");
+      wordSearched[0] = wordSearched[0].toUpperCase();
+      wordSearched = wordSearched.join("");
+      strAux[indexOfSearch] = wordSearched;
+    } else {
+      strAux[indexOfSearch] = after;
+    }
+  }
+
+  strAux = strAux.join(" ");
+  return strAux;
+}
+
+myReplace("Let us go to the store", "store", "mall");
+myReplace("He is Sleeping on the couch", "Sleeping", "sitting");
+// should return "He is Sitting on the couch"
+
+// Aditional solution using slice(1) and charAt(0)
+function myReplace(str, before, after) {
+  // Find index where before is on string
+  var index = str.indexOf(before);
+  // Check to see if the first letter is uppercase or not
+  if (str[index] === str[index].toUpperCase()) {
+    // Change the after word to be capitalized before we use it.
+    after = after.charAt(0).toUpperCase() + after.slice(1);
+  } else {
+    // Change the after word to be uncapitalized before we use it.
+    after = after.charAt(0).toLowerCase() + after.slice(1);
+  }
+  // Now replace the original str with the edited one.
+  str = str.replace(before, after);
+
+  return str;
+}
+
+// Another Solution using substring and replace
+function myReplace(str, before, after) {
+  // Check if first character of argument "before" is a capital or lowercase letter and change the first character of argument "after" to match the case
+  if (/^[A-Z]/.test(before)) {
+    after = after[0].toUpperCase() + after.substring(1);
+  } else {
+    after = after[0].toLowerCase() + after.substring(1);
+  }
+
+  // return string with argument "before" replaced by argument "after" (with correct case)
+  return str.replace(before, after);
+}
